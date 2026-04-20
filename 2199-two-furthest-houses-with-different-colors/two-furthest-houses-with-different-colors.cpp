5113981509e1b51bd6199e20fd36078a48cdad28
Atish Kumar <yadavatish2809@gmail.com>
2026-04-21 03:27:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (41.51%) - LeetSync

---
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n=colors.size();
+        int res=0;
+        for(int i=0;i<n;i++){
+            for(int j=n-1;j>i;j--){
+                if(colors[i]!=colors[j]){
+                    res=max(res,j-i);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
