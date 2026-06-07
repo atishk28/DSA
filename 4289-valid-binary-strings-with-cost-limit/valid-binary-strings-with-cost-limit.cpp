9987da5ca392b8e1ab436dee07eaced1ea663f0b
Atish Kumar <yadavatish2809@gmail.com>
2026-06-08 02:46:54 +0530

    Time: 12 ms (100.00%) | Memory: 34 MB (100.00%) - LeetSync

---
+++ 4289-valid-binary-strings-with-cost-limit/valid-binary-strings-with-cost-limit.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    vector<string> ans;
+
+    bool f2(string &s, int k) {
+        int sum = 0;
+
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '1') {
+                sum += i;
+                if(sum > k) return false;
+            }
+        }
+
+        return true;
+    }
+
+    void f(int n, string &s, int l, int k) {
+
+        if(l == n) {
+            if(f2(s, k))
+                ans.push_back(s);
+            return;
+        }
+
+        // Always place 0
+        s.push_back('0');
+        f(n, s, l + 1, k);
+        s.pop_back();
+
+        // Place 1 only if previous is not 1
+        if(s.empty() || s.back() != '1') {
+            s.push_back('1');
+            f(n, s, l + 1, k);
+            s.pop_back();
+        }
+    }
+
+public:
+    vector<string> generateValidStrings(int n, int k) {
+        string s = "";
+        f(n, s, 0, k);
+        return ans;
+    }
+};
