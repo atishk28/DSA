8931deafb9c00cbcc2a034bcf452d10a65c2aa40
Atish Kumar <yadavatish2809@gmail.com>
2026-07-13 03:35:56 +0530

    Time: 0 ms (100.00%) | Memory: 54.6 MB (19.97%) - LeetSync

--- 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.cpp
+++ 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.cpp
@@ -5,10 +5,7 @@ public:
         vector<int> res(n,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==1 && i<j){
-                    res[i]++;
-                    res[j]++;
-                }
+                res[i] += matrix[i][j];
             }
         }   
         return res;
