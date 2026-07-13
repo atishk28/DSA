625418244d3c189eec3b5d477d637bff8ee07a17
Atish Kumar <yadavatish2809@gmail.com>
2026-07-14 04:08:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (49.31%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> all={12,23,34,45,56,67,78,89,123,234,345,456,567,678,789,1234,2345,3456,4567,5678,6789,12345,23456,34567,45678,56789,123456,234567,345678,456789,1234567,2345678,3456789,12345678,23456789,123456789};
+        vector<int> result;
+        int n=all.size();
+        for (int i=0;i<n;i++){
+            if (all[i]<low) continue;
+            if (all[i]>high) break;
+            result.push_back(all[i]);
+        }
+        return result;
+    }
+};
